2d41f85dba3b8186cbac616586e6e0302ec14a03
dileepdivakaran666@gmail.com <dileepdivakaran666@gmail.com>
2025-03-22 04:26:47 +0530

    logout changess

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://dns-backend-m8hq.onrender.com'; 
-const API_BASE_URL = 'http://localhost:5000'; 
+const API_BASE_URL = 'https://dns-backend-m8hq.onrender.com'; 
+// const API_BASE_URL = 'http://localhost:5000'; 
 
 interface LoginResponse {
     token: string;
