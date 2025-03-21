37ae10cec27bee01b2b7e5c3a77d1a18eae0a035
dileepdivakaran666@gmail.com <dileepdivakaran666@gmail.com>
2025-03-22 01:33:56 +0530

    base url change commit

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // Replace with your actual API base URL
+const API_BASE_URL = 'https://dns-backend-m8hq.onrender.com'; // Replace with your actual API base URL
 
 interface LoginResponse {
     token: string;
